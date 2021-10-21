ef24bd2bab4a2b6b15f7ef57066485bd1a925078
BenYoo-ben <neverknown0909@gmail.com>
2021-10-21 09:13:31 +0100

    + receiving conn finish. To-Do: Send req. to desired redirected URL and stream it to client

--- captive_portal.c
+++ captive_portal.c
@@ -1,16 +1,8 @@
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <pthread.h>
+#include "captive_portal.h"
 
-#define __CAPTIVE_PORTAL_PORT__ 55555
-#define __CAPTIVE_PORTAL_WAIT_MAX__ 10
 
 pthread_mutex_t captive_portal_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t captive_portal_cond = PTHREAD_COND_INITIALIZER;
-
-void *captive_portal_handler(void *sock_arg);
 
 int main(){
 	
@@ -25,6 +17,9 @@ int main(){
 	struct sockaddr_in serv_addr;
 	
 	memset(&serv_addr,0,sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(__CAPTIVE_PORTAL_PORT__);
 
 	if( bind(server_socket, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 	{
@@ -41,15 +36,21 @@ int main(){
 	while(1){
 		struct sockaddr_in client_addr;
 
+		printf("waiting for input...\n");
 		int client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_size);
 		if(client_socket < 0){
 			printf("client socket accept fail \n");
+		}else{	
+			printf("Connected.\n");
 		}
-
+		
+		int *pointer_to_client_socket = malloc(sizeof(int));
+		*pointer_to_client_socket = client_socket;
+			
 		pthread_t new_thread;
 
 
-		pthread_create(&new_thread, NULL, captive_portal_handler, (void *)&client_addr);
+		pthread_create(&new_thread, NULL, captive_portal_handler, pointer_to_client_socket);
 
 	}
 
@@ -58,6 +59,22 @@ int main(){
 
 void *captive_portal_handler(void *sock_arg){
 	int sock = *((int *)sock_arg);
-
-	printf("sock desc : %d\n",sock);
+	captive_portal_redirect_socket_to_url(sock,"www.naver.com");
+	free(sock_arg);
 }
+
+void captive_portal_redirect_socket_to_url(int socket, const char *url){
+	
+	char reply[__CAPTIVE_PORTAL_MTU__];
+	memset(reply,0x0,__CAPTIVE_PORTAL_MTU__);
+	sprintf(reply, "HTTP/1.1 302 Found\r\n");
+	sprintf(reply, "Location: https://www.naver.com/ \r\n");
+
+	char *data = malloc(strlen(reply));
+	strcpy(data,reply);
+
+	write(socket,data,strlen(data));
+	printf("data sent . \n");
+	free(data);
+}
+
